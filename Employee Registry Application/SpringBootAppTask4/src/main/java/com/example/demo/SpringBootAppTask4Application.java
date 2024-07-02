deb7c65b8c5395e7d3a085a4af90626d0e697b7d
saibhavanamarada <saibhavanamarada.99@gmail.com>
2024-07-02 18:15:17 -0500

    Add files via upload

---
+++ Employee Registry Application/SpringBootAppTask4/src/main/java/com/example/demo/SpringBootAppTask4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppTask4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppTask4Application.class, args);
+	}
+
+}
